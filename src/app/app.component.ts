7b1abf844f740093adef2c97b2623ce52243dc59
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2022-10-11 01:10:17 -0300

    Inclusão de one-way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'alurapic';
+  description = "Leao";
+  url="https://www.infoescola.com/wp-content/uploads/2017/04/leao-126767138.jpg"
 }
