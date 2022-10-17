ab9120459b46b366f72a81eca525248a5f52fc47
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2022-10-16 21:41:53 -0300

    inclusão de input nos camposdo photo component

--- src/app/photo/photo.component.ts
+++ src/app/photo/photo.component.ts
@@ -1,10 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
     selector: 'app-photo',
     templateUrl: 'photo.component.html'
 })
 export class PhotoComponent{
-    description = "Leao";
-    url="https://st.depositphotos.com/1020341/4233/i/450/depositphotos_42333899-stock-photo-portrait-of-huge-beautiful-male.jpg"
+    @Input() description = "";
+    @Input() url="";
 }
