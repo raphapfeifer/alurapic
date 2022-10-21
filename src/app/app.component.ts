dda6c3c142cacd5ed51ecd47f05ef38f4faedbe5
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2022-10-21 00:43:46 -0300

    Inclusão de array de photos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+
+  photos = [
+    {
+      url: "https://st.depositphotos.com/1020341/4233/i/450/depositphotos_42333899-stock-photo-portrait-of-huge-beautiful-male.jpg",
+      description: "Leão"
+    },
+    {
+      url:"https://cdn.cloudcoaching.com.br/wp-content/uploads/2019/01/os-tres-leoes-uma-historia-sobre-superacao-e-lideranca-1200x628.png.webp",
+      description: "Leões"
+    }
+  ];
 }
