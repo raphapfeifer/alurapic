8722db14ce8c3c43d6f6ba18c2f35cd72d2e364e
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2022-11-07 00:28:05 -0300

    Segmento de rota variável

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { PhotoService } from '../photo/photo.service';
 
 @Component({
@@ -10,11 +11,15 @@ export class PhotoListComponent implements OnInit {
 
   photos: Object[] = [];
   
-constructor(private photoService: PhotoService){}
+constructor(
+  private photoService: PhotoService,
+  private activatedRoute: ActivatedRoute ){}
 
     ngOnInit(): void{
+
+      const userName = this.activatedRoute.snapshot.params.userName;
       this.photoService
-        .listFromUser('flavio')
+        .listFromUser(userName)
         .subscribe(
           photos => {
             console.log(photos[1].description);
