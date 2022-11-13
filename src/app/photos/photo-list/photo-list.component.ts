1455b4ed8d66abbc8280782e1b53729f465e255a
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2022-11-13 13:25:15 -0300

    introdução do campo filter

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -11,6 +11,7 @@ import { PhotoService } from '../photo/photo.service';
 export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = [];
+  filer: string = '';
   
 constructor(
   private photoService: PhotoService,
