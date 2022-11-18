630565ce43307b77882aaeae760cbaadaabe9cc8
raphael.alcateia <raphael.alcateia@salcisne.com.br>
2022-11-18 17:46:47 -0300

    Inclusão de loadbutton

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -7,13 +7,15 @@ import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
 import { PhotosComponent } from './photo-list/photos/photos.component';
 import { FilterByDescription } from "./photo-list/filter-by-description.pipe";
+import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
 
 @NgModule({
     declarations:[PhotoComponent,
                  PhotoListComponent,
                  PhotoFormComponent,
                  PhotosComponent,
-                FilterByDescription],
+                FilterByDescription,
+                LoadButtonComponent],
     imports:[HttpClientModule,
             CommonModule]
 })
