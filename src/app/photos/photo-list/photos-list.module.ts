ca700da3a8da44efbe7c91f33b51dcacb91746f1
raphael.alcateia <raphael.alcateia@salcisne.com.br>
2022-11-22 16:21:24 -0300

    Inclusão de card

--- src/app/photos/photo-list/photos-list.module.ts
+++ src/app/photos/photo-list/photos-list.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { CardModule } from "src/app/shared/components/card/card.module";
 import { PhotoModule } from "../photo/photo.module";
 import { FilterByDescription } from "./filter-by-description.pipe";
 import { LoadButtonComponent } from "./load-button/load-button.component";
@@ -13,6 +14,7 @@ import { PhotosComponent } from "./photos/photos.component";
                     FilterByDescription
                 ],
     imports:[CommonModule,
-             PhotoModule]            
+             PhotoModule,
+             CardModule]            
 })
 export class PhotoListModule{}
