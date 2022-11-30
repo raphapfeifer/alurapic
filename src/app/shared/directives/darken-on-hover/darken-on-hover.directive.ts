a32260699756e4e3c74307dde887b4d2f6c5577c
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2022-11-29 23:57:27 -0300

    finalização da tela

--- src/app/shared/directives/darken-on-hover/darken-on-hover.directive.ts
+++ src/app/shared/directives/darken-on-hover/darken-on-hover.directive.ts
@@ -1,6 +1,23 @@
-import { Directive } from "@angular/core";
+import { Directive, ElementRef, HostListener, Input, Renderer } from "@angular/core";
 
 @Directive({
     selector: '[apDarkenOnHover]'
 })
-export class DarkenOnHoverDirective{}
+export class DarkenOnHoverDirective{
+
+    @Input() brightness = '70%';
+
+    constructor(
+        private el: ElementRef,
+        private render: Renderer){}
+
+    @HostListener('mouseover')
+    darkenOn(){
+        this.render.setElementStyle(this.el.nativeElement, 'filter', `brightness(${this.brightness})`);
+    }
+
+    @HostListener('mouseleave')
+    darkenOff(){
+        this.render.setElementStyle(this.el.nativeElement, 'filter', 'brightness(100%)');
+    }
+}
