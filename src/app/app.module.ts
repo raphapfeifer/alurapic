84a890aec10de13d31c4e74e0d6d4527aaf1ff82
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2022-12-03 01:22:56 -0300

    Criação de component de signin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
 import { AppRoutingModule } from './app.routing.module';
 import { ErrorsModule } from './errors/errors.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { ErrorsModule } from './errors/errors.module';
     BrowserModule,
     PhotosModule,
     AppRoutingModule,
-    ErrorsModule
+    ErrorsModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes,RouterModule } from '@angular/router';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { SignInComponent } from './home/signin/signin.component';
 
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
 
 const routes: Routes = [
+    {path: '', component: SignInComponent},
     {
     path: 'user/:userName',
     component: PhotoListComponent, 
@@ -15,7 +17,7 @@ const routes: Routes = [
     }
     },
     {path: 'p/add', component: PhotoFormComponent},
-    {path: '**', component: NotFoundComponent}
+    {path: '**', component: NotFoundComponent},
 ];
 
 @NgModule({
