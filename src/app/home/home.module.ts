d73aeb21ee49a17850c7640cd9b25bf577256ab8
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2022-12-06 09:34:04 -0300

    Refatoração do formulário

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { SignInComponent } from "./signin/signin.component";
 import { ReactiveFormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
+import { VmessageModule } from "../shared/vmessage/vmessage.module";
 
 @NgModule({
     declarations:[SignInComponent],
-    imports: [CommonModule, ReactiveFormsModule]
+    imports: [CommonModule, ReactiveFormsModule, VmessageModule]
 })
 export class HomeModule{}
