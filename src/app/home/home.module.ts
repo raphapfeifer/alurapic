f8c7b1c236673c854a228f286b80522276a8c744
raphael.alcateia <raphael.alcateia@salcisne.com.br>
2022-12-08 15:33:40 -0300

    Autenticação de usuário usando router module

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,9 +3,10 @@ import { SignInComponent } from "./signin/signin.component";
 import { ReactiveFormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
 import { VmessageModule } from "../shared/vmessage/vmessage.module";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
     declarations:[SignInComponent],
-    imports: [CommonModule, ReactiveFormsModule, VmessageModule]
+    imports: [CommonModule, ReactiveFormsModule, VmessageModule, RouterModule]
 })
 export class HomeModule{}
--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
 import { AuthService } from "src/app/core/auth.service";
 
 @Component({
@@ -11,7 +12,8 @@ export class SignInComponent implements OnInit{
 
     constructor(
         private formBuilder: FormBuilder,
-        private authService: AuthService){}
+        private authService: AuthService,
+        private router: Router){}
     
     ngOnInit(): void {
         this.loginForm = this.formBuilder.group({
@@ -27,7 +29,7 @@ export class SignInComponent implements OnInit{
         this.authService
             .authenticate(userName,password)
             .subscribe(
-                () => console.log('autenticado'),
+                () => this.router.navigate(['user', userName]),
                 err => {
                     console.log(err);
                     this.loginForm.reset();
