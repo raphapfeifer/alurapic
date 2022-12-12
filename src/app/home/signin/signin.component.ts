7cc4dd9927878c3b3aa3fde469a60f76fc22bee1
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2022-12-12 13:04:39 -0300

    criação de focus em caso de login errado

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 import { AuthService } from "src/app/core/auth.service";
@@ -9,6 +9,7 @@ import { AuthService } from "src/app/core/auth.service";
 export class SignInComponent implements OnInit{
 
     loginForm: FormGroup;
+    @ViewChild('userNameInput') userNameInput: ElementRef<HTMLInputElement>;
 
     constructor(
         private formBuilder: FormBuilder,
@@ -33,6 +34,7 @@ export class SignInComponent implements OnInit{
                 err => {
                     console.log(err);
                     this.loginForm.reset();
+                    this.userNameInput.nativeElement.focus();
                 }
             );
     }
