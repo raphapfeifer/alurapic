f9f980f892721b7e457347bbc24a3c510dbf997e
raphael.alcateia <raphael.alcateia@salcisne.com.br>
2022-12-19 15:05:53 -0300

    Inclusão da armazenamento do token de acesso no local storage

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { tap } from 'rxjs/operators';
+import { TokenService } from '../token/token.service';
 
 const API_URL = 'http://localhost:3000';
 
@@ -9,13 +10,16 @@ const API_URL = 'http://localhost:3000';
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+              private http: HttpClient,
+              private tokenService: TokenService) { }
 
   authenticate(userName: string, password: string){
     return this.http
     .post(API_URL + '/user/login', {userName,password}, {observe: 'response'})
     .pipe(tap(res => {
         const authToken = res.headers.get('x-access-token');
+        this.tokenService.setToken(authToken);
         console.log(authToken);
     }))
   }
