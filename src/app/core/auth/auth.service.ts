8769d38353e696e1d3c47ec460c56ceeb48b8877
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2022-12-19 13:03:20 -0300

    Método para pegar o token retornado pelo back

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
 
 const API_URL = 'http://localhost:3000';
 
@@ -11,7 +12,12 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   authenticate(userName: string, password: string){
-    return this.http.post(API_URL + '/user/login', {userName,password})
+    return this.http
+    .post(API_URL + '/user/login', {userName,password}, {observe: 'response'})
+    .pipe(tap(res => {
+        const authToken = res.headers.get('x-access-token');
+        console.log(authToken);
+    }))
   }
 
 }
