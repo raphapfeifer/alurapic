9fb8009aa84cf57a1860db5d40819b40af3c8cd2
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2023-01-01 23:48:52 -0300

    Alteração para behavior subject

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,9 +1,20 @@
 import { Component } from "@angular/core";
+import { Observable } from "rxjs";
+import { User } from "../user/user";
+import { UserService } from "../user/user.service";
 
 @Component({
     selector: 'ap-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent{
+
+    user$: Observable<User>
+    user: User;
+
+    constructor(private userService: UserService){
+        this.user$ = userService.getUser();
+        this.user$.subscribe(user => this.user = user);
+    }
     
 }
--- src/app/core/user/user.service.ts
+++ src/app/core/user/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { Subject } from "rxjs";
+import { BehaviorSubject } from "rxjs";
 import { TokenService } from "../token/token.service";
 import { User } from "./user";
 import * as jtw_decode from "jwt-decode";
@@ -7,7 +7,8 @@ import * as jtw_decode from "jwt-decode";
 @Injectable({providedIn: 'root'})
 export class UserService{
 
-    private userSubject = new Subject<User>();
+    private userSubject = new BehaviorSubject<User>(null);
+
     constructor(private tokenService: TokenService){
         this.tokenService.hasToken() && 
             this.decodeAndNotify();
