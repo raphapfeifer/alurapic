d80f0e2bd3254d45bfea9380802985647140c2de
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2023-01-06 01:49:54 -0300

    Criação de método de logout

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 import { Observable } from "rxjs";
 import { User } from "../user/user";
 import { UserService } from "../user/user.service";
@@ -7,12 +8,17 @@ import { UserService } from "../user/user.service";
     selector: 'ap-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent{
+export class HeaderComponent {
 
     user$: Observable<User>
 
-    constructor(private userService: UserService){
+    constructor(private userService: UserService, private router: Router) {
         this.user$ = userService.getUser();
     }
-    
+
+    logout() {
+        this.userService.logout();
+        this.router.navigate(['']);
+    }
+
 }
