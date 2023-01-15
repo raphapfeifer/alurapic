2c221f99544e7d26edc5cf1cb0d0ebed0db9c8d7
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2023-01-15 18:57:45 -0300

    criação do primeiro validador

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { SignInComponent } from "./signin/signin.component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
-import { VmessageModule } from "../shared/vmessage/vmessage.module";
+
 import { RouterModule } from "@angular/router";
 import { SignUpComponent } from "./signup/signup.component";
+import { VmessageModule } from "../shared/components/vmessage/vmessage.module";
 
 @NgModule({
     declarations:[SignInComponent, SignUpComponent],
--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { lowerCaseValidator } from "src/app/shared/validators/lower-case-validator";
 
 @Component({
     templateUrl: './signup.component.html'
@@ -28,7 +29,8 @@ export class SignUpComponent implements OnInit{
         ],
             userName: ['', [
                 Validators.required,
-                Validators.pattern(/ˆ[a-z0-9_\-]+$/),
+                //Validators.pattern(/ˆ[a-z0-9_\-]+$/),
+                lowerCaseValidator,
                 Validators.minLength(2),
                 Validators.maxLength(30)
             ]
