95e38f7ebc614eab68717867e49da0f69212b7bc
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2023-02-25 04:49:20 -0300

    método de upload

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'ap-photo-form',
@@ -11,7 +13,10 @@ export class PhotoFormComponent implements OnInit {
   photoForm: FormGroup;
   file: File;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private photoService: PhotoService,
+    private router: Router) { }
 
   ngOnInit() {
     this.photoForm = this.formBuilder.group({
@@ -24,9 +29,9 @@ export class PhotoFormComponent implements OnInit {
   upload(){
     const description = this.photoForm.get('description').value;
     const allowComments = this.photoForm.get('allowComments').value;
-    console.log(description);
-    console.log(allowComments);
-    console.log(this.file);
+    this.photoService
+      .upload(description,allowComments, this.file)
+      .subscribe(() => this.router.navigate(['']))
   }
 
 }
--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
 import { VmessageModule } from "src/app/shared/components/vmessage/vmessage.module";
 import { PhotoFormComponent } from "./photo-form.component";
 
 @NgModule({
     declarations: [PhotoFormComponent],
-    imports:[CommonModule,ReactiveFormsModule,VmessageModule, FormsModule]
+    imports:[CommonModule,ReactiveFormsModule,VmessageModule, FormsModule, RouterModule]
 })
 export class PhotoFormModule{}
