e9f792e7ee1ccba4e2b97cb2d45bdb20449f1897
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2023-03-05 23:17:40 -0300

    Criação do preview

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
 import { VmessageModule } from "src/app/shared/components/vmessage/vmessage.module";
+import { PhotoModule } from "../photo/photo.module";
 import { PhotoFormComponent } from "./photo-form.component";
 
 @NgModule({
     declarations: [PhotoFormComponent],
-    imports:[CommonModule,ReactiveFormsModule,VmessageModule, FormsModule, RouterModule]
+    imports:[CommonModule,ReactiveFormsModule,VmessageModule, FormsModule, RouterModule,PhotoModule]
 })
 export class PhotoFormModule{}
