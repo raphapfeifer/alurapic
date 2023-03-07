effc679af0ae924346273b624701291371172cff
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2023-03-07 00:38:04 -0300

    Criação de diretiva para click

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
 import { VmessageModule } from "src/app/shared/components/vmessage/vmessage.module";
+import { ImmediateClickModule } from "src/app/shared/directives/immediate-click/immediate-click.module";
 import { PhotoModule } from "../photo/photo.module";
 import { PhotoFormComponent } from "./photo-form.component";
 
 @NgModule({
     declarations: [PhotoFormComponent],
-    imports:[CommonModule,ReactiveFormsModule,VmessageModule, FormsModule, RouterModule,PhotoModule]
+    imports:[CommonModule,ReactiveFormsModule,VmessageModule, FormsModule, RouterModule,PhotoModule, ImmediateClickModule]
 })
 export class PhotoFormModule{}
