3068feab030ec24eb3c6315b1d8e1c3db4c5aac7
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2023-03-15 21:44:12 -0300

    Criação de serviço para buscar comentários

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { Observable } from "rxjs";
 import { Photo } from "../photo/photo";
+import { PhotoComment } from "../photo/photo-comment";
 import { PhotoService } from "../photo/photo.service";
 
 @Component({
@@ -11,14 +12,16 @@ import { PhotoService } from "../photo/photo.service";
 export class PhotoDetailsComponent implements OnInit{
 
     photo$: Observable<Photo>;
+    comments$: Observable<PhotoComment[]>
 
     constructor(private route: ActivatedRoute,
         private photoService: PhotoService){}
 
 
     ngOnInit(): void {
-        const id = this.route.snapshot.params.photoId;
-        this.photo$ = this.photoService.findById(id);       
+        const photoId = this.route.snapshot.params.photoId;
+        this.photo$ = this.photoService.findById(photoId);
+        this.comments$ = this.photoService.getComments(photoId);       
     }
 
 }
