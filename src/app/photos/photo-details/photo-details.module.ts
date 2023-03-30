8a1ae014076f613e72a0cb89bb627c2a5dd25b69
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2023-03-30 00:20:40 -0300

    diretiva para validar se o usuário pode deletar foto

--- src/app/photos/photo-details/photo-details.module.ts
+++ src/app/photos/photo-details/photo-details.module.ts
@@ -6,9 +6,10 @@ import { VmessageModule } from "src/app/shared/components/vmessage/vmessage.modu
 import { PhotoModule } from "../photo/photo.module";
 import { PhotoCommentsComponent } from "./photo-comments/photo-comments.component";
 import { PhotoDetailsComponent } from "./photo-details.component";
+import { PhotoOwnerOnlyDirective } from "./photo-owner-only/photo-owner-only.directive";
 
 @NgModule({
-    declarations: [PhotoDetailsComponent, PhotoCommentsComponent],
+    declarations: [PhotoDetailsComponent, PhotoCommentsComponent, PhotoOwnerOnlyDirective],
     exports: [PhotoDetailsComponent, PhotoCommentsComponent],
     imports: [CommonModule,PhotoModule,RouterModule, ReactiveFormsModule, VmessageModule]
 })
