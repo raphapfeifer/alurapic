85849e9f7718beb190450fb084481bf0d897f55b
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2023-04-10 10:42:00 -0300

    diretiva para não aparecer likes quando o usuário não estiver logado

--- src/app/photos/photo-details/photo-details.module.ts
+++ src/app/photos/photo-details/photo-details.module.ts
@@ -7,10 +7,11 @@ import { PhotoModule } from "../photo/photo.module";
 import { PhotoCommentsComponent } from "./photo-comments/photo-comments.component";
 import { PhotoDetailsComponent } from "./photo-details.component";
 import { PhotoOwnerOnlyDirective } from "./photo-owner-only/photo-owner-only.directive";
+import { ShowIfLoggedModule } from "src/app/shared/directives/show-if-logged/show-if-logged.module";
 
 @NgModule({
     declarations: [PhotoDetailsComponent, PhotoCommentsComponent, PhotoOwnerOnlyDirective],
     exports: [PhotoDetailsComponent, PhotoCommentsComponent],
-    imports: [CommonModule,PhotoModule,RouterModule, ReactiveFormsModule, VmessageModule]
+    imports: [CommonModule,PhotoModule,RouterModule, ReactiveFormsModule, VmessageModule, ShowIfLoggedModule]
 })
 export class PhotoDetailsModule{}
